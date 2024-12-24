2010e88d0d8cad6e90d801efbca0c5bec18caccc
Success-akinyemi <successakinyemi365@gmail.com>
2024-12-24 13:51:20 +0100

    hee

--- src/Helpers/BaseUrl.js
+++ src/Helpers/BaseUrl.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const baseURL = import.meta.env.VITE_SERVER_URL;
+const baseURL = import.meta.env.VITE_SERVER_URL;
 //const baseURL = 'https://edu-connect-admin-server.onrender.com/api';
-const baseURL = 'http://localhost:9000/api'
+//const baseURL = 'http://localhost:9000/api'
 
 // Set the default base URL for axios
 axios.defaults.baseURL = baseURL;
